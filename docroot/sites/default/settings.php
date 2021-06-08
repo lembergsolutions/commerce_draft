dc0f316377f9344fdf661641ddc0feb9c739df55
Mykhailo Gurei <mykhailo.gurei@lemberg.co.uk>
2021-06-08 14:58:20 +0300

    Fix settings file

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -8,11 +8,6 @@ $settings['config_sync_directory'] = $app_root . '/../config/default';
 // Installation profile.
 $settings['install_profile'] = 'draft';
 
-// Include Platform.sh settings if available.
-if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
-  include $app_root . '/' . $site_path . '/settings.platformsh.php';
-}
-
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
