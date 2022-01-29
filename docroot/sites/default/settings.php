7eb19370f97c6a7d324dc850a0fd0f807dfa1225
Mykhailo Gurei <mykhailo.gurei@lemberg.co.uk>
2022-01-29 19:52:59 +0200

    Remove unused types

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -5,9 +5,6 @@ require_once 'default.settings.php';
 // This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = $app_root . '/../config/default';
 
-// Installation profile.
-$settings['install_profile'] = 'draft';
-
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
